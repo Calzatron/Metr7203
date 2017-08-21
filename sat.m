c26d852e65b16e86ae664e02089d52c9d3b98e30
Callum Rohweder <callumrohweder@gmail.com>
2017-08-21 23:48:39 +1000

    did question 2 assignment 2

---
+++ sat.m
@@ -0,0 +1,16 @@
+function output = sat( input )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+if input > 1
+    output = 1;
+elseif input < 0
+    output = 0;
+else
+    output = input;
+    
+end
+
+end
+
